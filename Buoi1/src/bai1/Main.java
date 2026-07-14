05cedcb07e73b2d0b852e201b43978ccc3534d78
VanTruong <vantruonghaui2007@gmail.com>
2026-07-14 18:20:45 +0700

    new updates

--- Buoi1/src/bai1/Main.java
+++ Buoi1/src/bai1/Main.java
@@ -2,6 +2,7 @@ package bai1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nhap n");
+        System.out.println("Nhap n : ");
+        System.out.println()
     }
 }
